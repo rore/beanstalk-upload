7b708ecc11fcf25ab15fe488bebc9ef921a05d5c
Rotem Hermon <rotem.hermon@gmail.com>
2012-03-08 21:03:21 +0200

    fixed s3 uploading

--- beanstalkUpload.groovy
+++ beanstalkUpload.groovy
@@ -1,11 +1,12 @@
 @GrabResolver(name='central', root='http://repo1.maven.org/maven2')
 @GrabResolver(name='javanet', root='http://download.java.net/maven/2')
-@Grab(group='com.amazonaws', module='aws-java-sdk', version='1.1.6')
+@GrabResolver(name='codehaus', root='http://repository.codehaus.org')
+@Grab(group='com.amazonaws', module='aws-java-sdk', version='1.3.0')
 @Grab(group='commons-logging', module='commons-logging', version='1.1.1')
-@Grab(group='commons-codec', module='commons-codec', version='1.4')
+@Grab(group='commons-codec', module='commons-codec', version='1.6')
 @Grab(group='commons-httpclient', module='commons-httpclient', version='3.1')
-@Grab(group='javax.mail', module='mail', version='1.4.3')
-@Grab(group='org.codehaus.jackson', module='jackson-core-asl', version='1.4.3')
+@Grab(group='javax.mail', module='mail', version='1.4.4')
+@Grab(group='org.codehaus.jackson', module='jackson-core-asl', version='1.9.4')
 @Grab(group='stax', module='stax', version='1.2.0')
 @Grab(group='stax', module='stax-api', version='1.0.1')
 
@@ -30,11 +31,9 @@ import com.amazonaws.services.elasticbeanstalk.AWSElasticBeanstalkClient
 import com.amazonaws.services.elasticbeanstalk.model.S3Location
 import com.amazonaws.services.elasticbeanstalk.model.CreateApplicationVersionRequest
 import com.amazonaws.services.elasticbeanstalk.model.ApplicationVersionDescription
+import com.amazonaws.services.s3.model.PutObjectRequest
 
-void upload(appName, appVersion, fileToUpload) {
-	
-	def fileSize = fileToUpload.size()
-	def config = new Properties()
+void upload(appName, bucketName, appDesc, fileToUpload) {
 	
 	def credentialsFile = new File("credentials.properties")
 	if (!credentialsFile.exists()) {
@@ -49,52 +48,43 @@ void upload(appName, appVersion, fileToUpload) {
 	def credentials = new PropertiesCredentials(credentialsFile)
 	log(appName, "Loaded AWS credentials")
 	
-	def s3client = new AmazonS3Client(credentials) 
-	def bucketName = "${appName}-${UUID.randomUUID()}"
-    
-	log(appName, "Creating s3 bucket '${bucketName}' to hold application file")
-	def bucket = s3client.createBucket(bucketName)
-	
 	def objectKey = "${new Date().format('yyyyMMddHHmmss')}-${fileToUpload.name}"
+	def appVersion = "${new Date().format('yyyyMMddHHmmss')}"
 	
-	def metadata = new ObjectMetadata()
-	metadata.setContentLength(fileSize) 
-	metadata.addUserMetadata("app-name", appName)
-	metadata.addUserMetadata("app-version", appVersion)	
-	
-	def transferManager = new TransferManager(credentials)
-	fileToUpload.withInputStream { fileToUploadInputStream -> 
-		
-		def decimalFormat = new java.text.DecimalFormat("##.#")
-		def warUpload = transferManager.upload(bucketName, objectKey, fileToUploadInputStream, metadata)
-		
-		while (!warUpload.done) {
-			
-			def percent = (warUpload.progress.bytesTransfered * 100) / warUpload.progress.totalBytesToTransfer
-			def percentToShow = decimalFormat.format(percent)
-			log(true, appName, "${warUpload.description}: [${warUpload.state}] - ${warUpload.progress.bytesTransfered} of ${warUpload.progress.totalBytesToTransfer} (${percentToShow}%) ")
-			Thread.sleep(1000)
-		}	
-	}
-			
-	def beanstalk = new AWSElasticBeanstalkClient(credentials)
+	// Upload a WAR file to Amazon S3
+    def warFile = fileToUpload
+	def s3 = new AmazonS3Client(credentials)
+    println "Uploading application to Amazon S3"
+ 	def metadata = new ObjectMetadata()
+	metadata.setContentType("application/x-zip")	
+	def putObjectRequest = new PutObjectRequest(bucketName, objectKey, warFile)
+	putObjectRequest.setMetadata(metadata)
+	def s3Result = s3.putObject(putObjectRequest)
+    //def s3Result = s3.putObject(bucketName, objectKey, warFile)
+    println "Uploaded application $s3Result.versionId"
+
+   def beanstalk = new AWSElasticBeanstalkClient(credentials)
 	def applicationVersionRequest = new CreateApplicationVersionRequest(appName, appVersion)
-	applicationVersionRequest.setAutoCreateApplication(true)
+	applicationVersionRequest.setAutoCreateApplication(false)
 	applicationVersionRequest.setSourceBundle(new S3Location(bucketName, objectKey))
+	applicationVersionRequest.setDescription(appDesc)
 	
 	println ""
 	log(appName, "Creating application version...")
 	def applicationVersionResult = beanstalk.createApplicationVersion(applicationVersionRequest)
 	def applicationVersionDescription = applicationVersionResult.applicationVersion
     
+	//println "Update environment with uploaded application version"
+    //def updateEnviromentRequest = new UpdateEnvironmentRequest(environmentName: environmentName, versionLabel: versionLabel)
+    //def updateEnviromentResult = elasticBeanstalk.updateEnvironment(updateEnviromentRequest)
+    //println "Updated environment $updateEnviromentResult"
+	
 	log(appName, "Done!")
 	log(appName, "App: ${applicationVersionDescription.applicationName}")
 	log(appName, "Version: ${applicationVersionDescription.versionLabel}")
 	log(appName, "S3 Bucket: ${bucketName}")
 	log(appName, "War file:  ${objectKey}")
 	log(appName, "Version created at: ${applicationVersionDescription.dateCreated.format('yyyy/MM/dd HH:mm:ss')}")
-	
-	transferManager.shutdownNow()
 }
 
 void log(message) {
@@ -116,18 +106,19 @@ def logValue = "org.apache.commons.logging.impl.NoOpLog"
 LogFactory.getFactory().setAttribute(logAttribute, logValue)
 
 //script
-if (args.size() != 3) {
-	log("Usage: groovy beanstalkUpload.groovy <path_to_war> <application_name> <application_version_label>")
+if (args.size() != 4) {
+	log("Usage: groovy beanstalkUpload.groovy <path_to_war> <s3bucket> <application_name> <application_description>")
 	System.exit(1)
 }
 
 def fileToUpload = new File(args[0])
-def appName = args[1].toLowerCase()
-def appVersion = args[2]
+def bucketName = args[1]
+def appName = args[2].toLowerCase()
+def appDesc = args[3]
 
 if (!fileToUpload.exists()) {
 	log("File '${fileToUpload}' does not exist")
 	System.exit(1)
 }
 
-upload(appName, appVersion, fileToUpload)
+upload(appName, bucketName, appDesc, fileToUpload)
